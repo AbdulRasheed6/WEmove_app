9ac2f3c61a2b4ddf2b37169066c1f4b6334bb2a6
AbdulRasheed6 <abdulrasheedolakiitan@gmail.com>
2021-05-11 19:24:36 +0100

    git commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('public'));
-app.use('/style.css', express.static(__dirname + 'public/style.css'));
+//app.use(express.static('public'));
+
+
 console.log(__dirname);
 
 //Ejs
@@ -12,10 +14,13 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 
+app.use( express.static(path.join(__dirname,  'public')));
+
 //Routes
 
 app.use('/', require('./routes/index'));
 app.use('/users',  require('./routes/users'))
+
 
 const PORT = process.env.PORT || 8080;
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // Login page
 router.get('/login', (req, res) => res.render('login'));
 router.get('/Homepage', (req, res) => res.render('Homepage'));
+
 // Register page
 router.get('/register', (req, res) => res.send('Register'))
 
